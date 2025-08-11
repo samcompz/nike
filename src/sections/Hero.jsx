4bd65350eed526f3277e4e010945c7a335883382
Samcompz <samwambua25@gmail.com>
2025-08-11 05:22:50 +0300

    testing and making amends

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -9,11 +9,9 @@ const Hero = () => {
   const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
 
   return (
-    <section
-      id='home'
-      className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
-    >
-      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28'>
+    <section id='home' className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'>
+
+      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-2 pl-7'>
         <p className='text-xl font-montserrat text-coral-red'>
           Our Summer collections
         </p>
@@ -23,7 +21,7 @@ const Hero = () => {
             The New Arrival
           </span>
           <br />
-          <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
+          <span className='text-red-800 inline-block mt-3'>Nike</span> Shoes
         </h1>
         <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>
           Discover stylish Nike arrivals, quality comfort, and innovation for
@@ -32,7 +30,7 @@ const Hero = () => {
 
         <Button label='Shop now' iconURL={arrowRight} />
 
-        <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
+        <div className='flex justify-start items-start flex-wrap w-full mt-2 gap-16'>
           {statistics.map((stat, index) => (
             <div key={index}>
               <p className='text-4xl font-palanquin font-bold'>{stat.value}</p>
